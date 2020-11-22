70d67e17aef75cf0b78dc89032a68621532ef857
Tony Lin <tonylinbusiness@gmail.com>
2020-11-22 17:29:29 -0500

    Created specification evaluator

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -6,7 +6,7 @@ namespace Core.Specifications
 {
     public class BaseSpecification<T> : ISpecification<T>
     {
-        public BaseSpecification(Expression<Func<T, bool>> criteria,)
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
         {
             Criteria = criteria;
         }
