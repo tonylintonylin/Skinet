660e4c714f855911b9b0f07db19b15d1c550887a
Tony Lin <tonylinbusiness@gmail.com>
2020-11-22 17:23:12 -0500

    Created specification class

---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        // where
+        Expression<Func<T, bool>> Criteria { get; }
+        // include()
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
