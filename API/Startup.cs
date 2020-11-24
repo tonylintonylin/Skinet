aa6ed2ac4a8b0a52b0f44ca24f40a5585debce32
Tony Lin <tonylinbusiness@gmail.com>
2020-11-24 08:24:17 -0500

    Added CORS support

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,15 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +54,7 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();
