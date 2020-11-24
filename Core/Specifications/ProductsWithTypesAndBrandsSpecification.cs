850751dda9d1e247054c985aa2a9d94c200f485d
Tony Lin <tonylinbusiness@gmail.com>
2020-11-23 20:19:57 -0500

    Added price sorting

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -12,6 +12,22 @@ namespace Core.Specifications
             AddInclude(x => x.ProductBrand);
 
             AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int id) 
