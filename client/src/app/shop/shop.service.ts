2f1b0964f76271571bae8122b97a5311f88ff120
Tony Lin <tonylinbusiness@gmail.com>
2020-11-25 10:24:33 -0500

    Consuming services in component

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPagination } from '../shared/models/pagination';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IPagination> {
+    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
 }
