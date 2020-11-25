758531afe7bc111f8ebe366687d1e1a54107eaf5
Tony Lin <tonylinbusiness@gmail.com>
2020-11-25 10:58:27 -0500

    Added product filters to service, consumed

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IBrand } from '../shared/models/brands';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,13 @@ export class ShopService {
 
   getProducts(): Observable<IPagination> {
     return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands(): Observable<IBrand[]> {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes(): Observable<IType[]> {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
