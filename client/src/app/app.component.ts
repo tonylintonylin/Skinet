a1a2083170b9cec39d237c011b2c62c5c68280c9
Tony Lin <tonylinbusiness@gmail.com>
2020-11-25 09:38:13 -0500

    Created initial interfaces

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'skinet';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: any) => {
+      (response: IPagination) => {
         this.products = response.data;
       },
       (error) => {
