d38c7aaae1b59b7fa64b31583636e9b7c03c50f9
Tony Lin <tonylinbusiness@gmail.com>
2020-11-29 11:37:43 -0500

    Persisting login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,16 +10,41 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'skinet';
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(
+        () => {
+          console.log('loaded user');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  loadBasket(): void {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
-      this.basketService.getBasket(basketId).subscribe(() => {
-        console.log('initialized basket');
-      }, error => {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialized basket');
+        },
+        (error) => {
           console.log(error);
-      })
+        }
+      );
     }
   }
 }
