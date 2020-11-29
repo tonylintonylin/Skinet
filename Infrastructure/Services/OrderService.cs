a37fdc78485dde5c3bb78b12edb9eb233d0ce6ff
Tony Lin <tonylinbusiness@gmail.com>
2020-11-28 19:03:09 -0500

    Refactored order service to use unit of work

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -11,55 +11,56 @@ namespace Infrastructure.Services
     {
         #region Dependency Injection
 
-        private readonly IGenericRepository<Order> _orderRepo;
-        private readonly IGenericRepository<DeliveryMethod> _dmRepo;
-        private readonly IGenericRepository<Product> _productRepo;
         private readonly IBasketRepository _basketRepo;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public OrderService(IGenericRepository<Order> orderRepo, 
-            IGenericRepository<DeliveryMethod> dmRepo, 
-            IGenericRepository<Product> productRepo,
-            IBasketRepository basketRepo
-            )
+        public OrderService(IBasketRepository basketRepo, IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _basketRepo = basketRepo;
-            _productRepo = productRepo;
-            _dmRepo = dmRepo;
-            _orderRepo = orderRepo;
         }
 
         #endregion Dependency Injection
 
-        public async Task<Order> CreateOrderAsync(string buyerEmail, 
+        public async Task<Order> CreateOrderAsync(string buyerEmail,
         int delvieryMethodId, string basketId, Address shippingAddress)
         {
             // Get basket from the repo
             var basket = await _basketRepo.GetBasketAsync(basketId);
+
             // Get items from the product repo
             var items = new List<OrderItem>();
             foreach (var item in basket.Items)
             {
-                var productItem = await _productRepo.GetByIdAsync(item.Id);
-                var itemOrdered = new ProductItemOrdered(productItem.Id, 
+                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+                var itemOrdered = new ProductItemOrdered(productItem.Id,
                     productItem.Name, productItem.PictureUrl);
 
                 var orderItem = new OrderItem(itemOrdered, productItem.Price,
                     item.Quantity);
                 items.Add(orderItem);
             }
-            // Get delivery method from repo
-            var deliveryMethod = await _dmRepo.GetByIdAsync(delvieryMethodId);
 
-            // calculate subtotal based on item price from product repo
+            // Get delivery method from repo
+            var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(delvieryMethodId);
+
+            // Calculate subtotal based on item price from product repo
             var subtotal = items.Sum(item => item.Price * item.Quantity);
 
-            // create order
+            // Create order
             var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+            _unitOfWork.Repository<Order>().Add(order);
 
-            // TODO: save to db
-            
-            // return order
-            return order; 
+            // Save to db
+            var result = await _unitOfWork.Complete();
+
+            if(result <= 0) return null;
+
+            // Delete basket
+            await _basketRepo.DeleteBasketAsync(basketId);
+
+            // Return order
+            return order;
         }
 
         public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
