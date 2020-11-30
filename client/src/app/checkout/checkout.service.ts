d14199d96b4294b3231e009b2c05a5c8742f2026
Tony Lin <tonylinbusiness@gmail.com>
2020-11-30 10:16:52 -0500

    creating delivery form

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IDeliveryMethod } from '../shared/models/deliveryMethods';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDeliveryMethods(): Observable<IDeliveryMethod[]> {
+    return this.http.get(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map((dm: IDeliveryMethod[]) => {
+        return dm.sort((a, b) => b.price - a.price);
+      })
+    );
+  }
 }
