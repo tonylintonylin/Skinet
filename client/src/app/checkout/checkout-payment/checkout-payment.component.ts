1491e03d7457688820c952802e15814bc0411628
Tony Lin <tonylinbusiness@gmail.com>
2020-11-30 11:58:28 -0500

    Routed order submission to checkout success page

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
@@ -17,7 +18,8 @@ export class CheckoutPaymentComponent implements OnInit {
   constructor(
     private basketService: BasketService,
     private checkoutService: CheckoutService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -29,7 +31,8 @@ export class CheckoutPaymentComponent implements OnInit {
       (order: IOrder) => {
         this.toastr.success('Order created succcesfully');
         this.basketService.deleteLocalBasket(basket.id);
-        console.log(order);
+        const navigationExtras: NavigationExtras = { state: order };
+        this.router.navigate(['checkout/success'], navigationExtras);
       },
       (error) => {
         this.toastr.error(error.message);
@@ -38,11 +41,13 @@ export class CheckoutPaymentComponent implements OnInit {
     );
   }
 
-  private getOrderToCreate(basket: IBasket): {
+  private getOrderToCreate(
+    basket: IBasket
+  ): {
     basketId: string;
     deliveryMethodId: number;
     shipToAddress: any;
-} {
+  } {
     return {
       basketId: basket.id,
       deliveryMethodId: +this.checkoutForm
