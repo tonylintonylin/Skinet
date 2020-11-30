52bc410a8c3b9d7ba7eef62fb079cce7c5a166b5
Tony Lin <tonylinbusiness@gmail.com>
2020-11-30 11:26:51 -0500

    user can save address

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
   templateUrl: './checkout-address.component.html',
-  styleUrls: ['./checkout-address.component.scss']
+  styleUrls: ['./checkout-address.component.scss'],
 })
 export class CheckoutAddressComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private accountSerivce: AccountService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saveUserAddress(): void {
+    this.accountSerivce
+      .updateUserAddress(this.checkoutForm.get('addressForm').value)
+      .subscribe(
+        () => {
+          this.toastr.success('Address saved');
+        },
+        (error) => {
+          this.toastr.error(error.message);
+          console.log(error);
+        }
+      );
   }
-
 }
